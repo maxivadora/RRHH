f5f274f68f575a98e160b98b643e7a6e71e393e2
Jose Luis Simeon <Jose Luis Simeon>
2017-04-15 12:00:09 -0300

    Todo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Quienes somos</div>
+
+                <div class="panel-body">
+
+                </div>
+
+            </div>
+
+            <div class="panel panel-default">
+
+                <div class="panel-heading">Objetivos</div>
+                <div class="panel panel-default">
+
+                    <div class="panel-heading">Usuarios</div>
+
+                    <div class="panel-body">
+
+                    </div>
+
+                </div>
+                <div class="panel-body">
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+        </div>
+    </div>
+</div>
+@endsection
