3832788ead0317b2324c4a3490609f9bffb16537
Maxi Vadora <maxivadora@gmail.com>
2017-04-15 21:21:23 -0300

    se completo seed de puestos

--- database/seeds/PuestosTableSeeder.php
+++ database/seeds/PuestosTableSeeder.php
@@ -19,5 +19,70 @@ class PuestosTableSeeder extends Seeder
             'deleted_at' => null,
             'depende_de' => null,
         ]);
+        DB::table('puestos')->insert([
+            'nombre' => 'Gerente General',
+            'descripcion' => 'Puesto maximo de la empresa',
+            'created_at' => null,
+            'updated_at' => null,
+            'deleted_at' => null,
+            'depende_de' => null,
+        ]);
+        DB::table('puestos')->insert([
+            'nombre' => 'Administracion de RRHH',
+            'descripcion' => 'Encargado del personal',
+            'created_at' => null,
+            'updated_at' => null,
+            'deleted_at' => null,
+            'depende_de' => '2',
+        ]);
+        DB::table('puestos')->insert([
+            'nombre' => 'Liquidacion de Sueldos',
+            'descripcion' => 'Encargado de pagar al personal',
+            'created_at' => null,
+            'updated_at' => null,
+            'deleted_at' => null,
+            'depende_de' => '3',
+        ]);
+        DB::table('puestos')->insert([
+            'nombre' => 'Dpto Ventas',
+            'descripcion' => 'Realiza las ventas',
+            'created_at' => null,
+            'updated_at' => null,
+            'deleted_at' => null,
+            'depende_de' => '2',
+        ]);
+        DB::table('puestos')->insert([
+            'nombre' => 'Marketing',
+            'descripcion' => 'Encargado de las relaciones externas',
+            'created_at' => null,
+            'updated_at' => null,
+            'deleted_at' => null,
+            'depende_de' => '2',
+        ]);
+        DB::table('puestos')->insert([
+            'nombre' => 'Dpto Produccion',
+            'descripcion' => 'No depende de ningun otro puesto',
+            'created_at' => null,
+            'updated_at' => null,
+            'deleted_at' => null,
+            'depende_de' => '2',
+        ]);
+        DB::table('puestos')->insert([
+            'nombre' => 'Embalaje',
+            'descripcion' => 'Encargado de embalar la mercaderia',
+            'created_at' => null,
+            'updated_at' => null,
+            'deleted_at' => null,
+            'depende_de' => '7',
+        ]);
+
+        DB::table('puestos')->insert([
+            'nombre' => 'Armado',
+            'descripcion' => 'Arma el producto',
+            'created_at' => null,
+            'updated_at' => null,
+            'deleted_at' => null,
+            'depende_de' => '7',
+        ]);
     }
 }
